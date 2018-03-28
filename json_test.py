a9f115ef745b173610e4757d9ea562e11ef46b28
Mariana Ababii <djpolmd@gmail.com>
2018-03-28 19:12:41 +0300

    message

--- json_test.py
+++ json_test.py
@@ -1,14 +1,21 @@
 import json
 from pprint import pprint
+import sys
+import argparse
 
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("size", type=int, help="Size of the puzzle's side. Must be >3.")
+	
+	random.seed()
 # Read in the file
-with open('data.json', 'r') as file :
-  filedata = file.read()
+	with open('data.json', 'r') as file :
+		filedata = file.read()
 
-# Replace the target string
-filedata = filedata.replace(',\\', ',')
-filedata = filedata.replace(',', ',\n')
-filedata = filedata.replace('}},','}},\n')
-# Write the file out again
-with open('data.json', 'w') as file:
-  file.write(filedata)
+	# Replace the target string
+	filedata = filedata.replace(',\\', ',')
+	filedata = filedata.replace(',', ',\n')
+	filedata = filedata.replace('}},','}},\n')
+	# Write the file out again
+	with open('data.json', 'w') as file:
+		file.write(filedata)
